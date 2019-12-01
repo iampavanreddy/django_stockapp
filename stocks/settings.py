b1d92b555f9c52e1f88df507b2b273dd2e37147b
Pavan Reddy <pa1reddy7299@gmail.com>
2019-12-01 14:50:45 +0530

    SECRET_KEY hidden

--- stocks/settings.py
+++ stocks/settings.py
@@ -22,7 +22,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'c2o#e#bmkta&%7n$)dvro3u!i+@bxskzqug6_d3jc190hn38sv'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
